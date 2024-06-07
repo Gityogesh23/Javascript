cd5cf896a59fea6567de9c25268ff03b2fe25123
Gityogesh23 <yogeshpatil1844@gmail.com>
2024-06-07 13:12:29 +0530

    initial commit

---
+++ first.js
@@ -0,0 +1,14 @@
+fname:'yogesh ';
+lname;'patil';
+age:29;
+console.log(fname);
+
+const family={
+village_name:'satod',
+code:123,
+state:'maharashtra',
+country:'India'
+};
+
+console.log(family.village_name);
+
